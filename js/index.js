bb0937859f833d6f675d9986a314ab45a8ea22b1
karimiyani <front_end2020@yahoo.com>
2021-10-24 00:58:15 +0330

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//class
+// programm
+
+
+// if we want create rectangle
+const x = 30 ;
+const y = 70 ;
+
+// =>  rectangle have property
+// x, y, color, ... their are property
+
+function getArea (){
+    return x * y;
+}
+// =>  rectangle have function too (method)
+
+class Rectangle {
+    //
+    //
+}
